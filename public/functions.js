ff8431b43df6670276889368d28a7008e2ec0371
carlos-santoscr <carloscruzsantos.ba96@gmail.com>
2021-04-17 18:01:10 -0300

    create calculadora

---
+++ public/functions.js
@@ -0,0 +1,22 @@
+function soma(){
+  var n1 = prompt( "digite o número 1");
+  var n2 = prompt("digite o número 2");
+ alert(parseInt(n1)+parseInt(n2));
+}
+
+function subtração(){
+  var n1 = prompt("digite o número 1");
+  var n2 = prompt("digite o número 2");
+ alert( parseInt(n1)-parseInt(n2));
+}
+function divisão(){
+  var n1 = prompt( "digite o número 1");
+  var n2 = prompt("digite o número 2");
+ alert(parseInt(n1)/parseInt(n2));
+}
+
+function multiplicação(){
+  var n1 = prompt("digite o número 1");
+  var n2 = prompt("digite o número 2");
+ alert( parseInt(n1)*parseInt(n2));
+}
